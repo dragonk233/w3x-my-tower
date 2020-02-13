9825adaf7e08e95a6ab4bbac9fa69ecbe0680ebc
hunzsig <mzyhaohaoren@qq.com>
2020-02-14 03:13:01 +0800

    up

---
+++ loader.lua
@@ -0,0 +1,12 @@
+--调试
+HLUA_DEBUG = false
+if (HLUA_DEBUG) then
+    console = require "jass.console"
+    console.enable = true
+end
+
+--加载h-lua
+require "h-lua"
+
+--加载game
+require "game"
--- main.lua
+++ main.lua
@@ -2,15 +2,4 @@
 package.path = package.path .. ";Z:\\war3\\hunzsig-warcraft3\\h-lua\\?.lua"
 package.path = package.path .. ";Z:\\war3\\hunzsig-warcraft3\\w3x-my-tower\\?.lua"
 
---调试
-HLUA_DEBUG = false
-if (HLUA_DEBUG) then
-    console = require "jass.console"
-    console.enable = true
-end
-
---加载h-lua
-require "h-lua"
-
---加载game
-require "game"
+require "loader"
